ef3e975f8720e5e8de59210def92b8be10ee0244
Dmutrenko <Dmutrenko@lab4.com>
2022-12-04 04:29:51 +0200

    AdvancedCalc class finished

---
+++ Program/AdvancedCalc.cs
@@ -0,0 +1,9 @@
+public class AdvancedCalc : Calculator
+{
+    public double num;
+
+    public double Log()
+    {
+        return Math.Log10(num);
+    }
+}
--- Program/Program.cs
+++ Program/Program.cs
@@ -5,8 +5,14 @@
         Calculator calculator = new Calculator();
 
         calculator.num1 = 7;
-        calculator.num2 = 3;
+        calculator.num2 = 3; 
 
         Console.WriteLine(calculator.Division());
+
+        AdvancedCalc advanced = new AdvancedCalc();
+
+        advanced.num = 5;
+
+        Console.WriteLine(advanced.Log())
     }
 }
