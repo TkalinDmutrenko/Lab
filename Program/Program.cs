face44e7c724d818387761de7bd83b17b2b5db87
Dmutrenko <Dmutrenko@lab4.com>
2022-12-04 05:18:39 +0200

    Console menu

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,17 +2,13 @@
 {
     public static void Main()
     {
+        Console.WriteLine("0 - Exit\n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\n5 - Log\n6 - FromKilometersToMiles");
+        string x = Console.ReadLine();
+        
         Calculator calculator = new Calculator();
 
         calculator.num1 = 7;
         calculator.num2 = 3; 
-
-        Console.WriteLine(calculator.Division());
-
-        AdvancedCalc advanced = new AdvancedCalc();
-
-        advanced.num = 5;
-
-        Console.WriteLine(advanced.Log());
+        calculator.num = 5;
     }
 }
