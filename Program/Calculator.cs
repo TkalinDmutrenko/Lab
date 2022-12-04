3e6fe1b00801a1af772aa912b6c2b42c4fc39dbf
Tkalin <artemtkalin13@gmail.com>
2022-12-04 05:15:58 +0300

    Finished basic class

---
+++ Program/Calculator.cs
@@ -0,0 +1,24 @@
+public class Calculator 
+{
+    public double num1, num2;
+
+    public double Addition()
+    {
+        return num1 + num2;
+    }
+
+    public double Subtraction()
+    {
+        return num1 - num2;
+    }
+
+    public double Multiplication()
+    {
+        return num1 * num2;
+    }
+
+    public double Division()
+    {
+        return num1 / num2;
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Calculator calculator = new Calculator();
+
+        calculator.num1 = 7;
+        calculator.num2 = 3;
+
+        Console.WriteLine(calculator.Division());
+    }
+}
